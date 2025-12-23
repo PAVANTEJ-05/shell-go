b7b12076644f0bad4212bc8670ad6a380ec794d6
PAVANTEJ-05 <nptpavantej79@gmail.com>
2025-12-24 00:31:11 +0530

    echo impl refactored

--- app/main.go
+++ app/main.go
@@ -16,18 +16,19 @@ func main() {
 	fmt.Print("$ ")
 
 	x,e:= bufio.NewReader(os.Stdin).ReadString('\n')
-	in :=strings.Split(x," ")[0]
-	arg,_ :=strings.CutPrefix(x,in+" ")
-		// fmt.Printf("in: %q \n arg: %q\n",in,arg)
+	in :=strings.TrimSpace(strings.Split(x," ")[0])
+	a:= strings.SplitAfter(x," ")[1:]
+	arg:= strings.Trim(strings.Join(a, "")," ")
+	// fmt.Printf("in: %q \n arg: %q\n",in,a)
 	if e!= nil{
 		fmt.Print(e) 
 		os.Exit(1)
 	}
 	switch in {
 	case "echo":
-			// i,_:= bufio.NewReader(os.Stdin).ReadString('\n')
-			fmt.Print(arg)
-	case "exit\n": os.Exit(0)
+		 fmt.Print(arg)
+	case "exit":
+		 os.Exit(0)
 	default:
 fmt.Print(in,": command not found\n")
 	}
