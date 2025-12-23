052504f616801b325ea83a6b916c04d661828aaf
PAVANTEJ-05 <nptpavantej79@gmail.com>
2025-12-23 21:27:09 +0530

    loop for shell

--- app/main.go
+++ app/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	// "bufio"
+	"os"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
@@ -9,9 +11,13 @@ var _ = fmt.Print
 
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
+ for{	
 	fmt.Print("$ ")
 	var x string
 	_,e:= fmt.Scan(&x)
-	if e== nil{ fmt.Print(x,": command not found")
-}else	{fmt.Print(e)}
+	if e!= nil{fmt.Print(e) 
+		os.Exit(1)
+	}
+fmt.Print(x,": command not found\n")
+}
 }
