276df68b0654d4a8bb938586dd468eb0f1d473b0
PAVANTEJ-05 <nptpavantej79@gmail.com>
2025-12-23 21:04:49 +0530

    usr input

--- app/main.go
+++ app/main.go
@@ -10,4 +10,8 @@ var _ = fmt.Print
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
 	fmt.Print("$ ")
+	var x string
+	_,e:= fmt.Scan(&x)
+	if e== nil{ fmt.Println(x,": command not found")
+}else	{fmt.Print(e)}
 }
